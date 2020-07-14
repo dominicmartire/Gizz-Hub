ea9e5b1833e907515be52b0a8d5950383ac8f148
Dom Martire <dmartire99@gmail.com>
2020-07-13 23:36:05 -0400

    serving frontend as static file, changed BrowserRouter to HashRouter to avoid client side vs server side routing conflicts

--- backend/app.js
+++ backend/app.js
@@ -6,18 +6,25 @@ const Video = require('./models/video')
 const crypto = require('crypto')
 const videoRouter = require('./controllers/videoPaths')
 const concertRouter = require('./controllers/concertPaths')
+const logger = require('./middleware/logger')
+
 require('dotenv').config()
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+app.use(logger.requestLogger)
+
 
 const mongo_uri ="mongodb+srv://dmartire99:Briarwood1!@cluster0-sc6g0.mongodb.net/gizz-hub?retryWrites=true&w=majority"
 mongoose.connect(mongo_uri, { useNewUrlParser: true })
     .then(()=>console.log("connected to mongodb"))
     .catch(error=>"error"+error.message)
 
+
+app.use(express.static('build'))
 app.use('/api/videos', videoRouter)
 app.use('/api/concerts', concertRouter)
+
 
 module.exports = app
